650d719cbbf297839cb9858fa8cfa546068b1283
Amine Grassi <grassi.amine@hotmail.fr>
2021-07-04 20:18:31 +0100

    Reactive Forms Validation Password

---
+++ src/app/validators/confirm-password.validator.ts
@@ -0,0 +1,19 @@
+import { AbstractControl, FormGroup } from "@angular/forms";
+export function ConfirmPasswordValidator(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+    const control = formGroup.controls[controlName];
+    const matchingControl = formGroup.controls[matchingControlName];
+    console.log('pasword=', control.value);
+    console.log('RePasword=', matchingControl.value);
+    if (matchingControl.errors && !matchingControl.errors.confirmedValidator) {
+      return;
+    }
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ confirmedValidator: true });
+      console.log('err1=', matchingControl.setErrors);
+    } else {
+      console.log('err2   =', matchingControl.setErrors);
+      matchingControl.setErrors(null);
+    }
+  }
+}
